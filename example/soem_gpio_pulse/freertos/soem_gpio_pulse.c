cca1c241d6b3bdc0a519906f1a812957df1a2f5c
qizhang <jony.zhang@nxp.com>
2023-02-01 22:24:12 +0800

    [MCUX-56704] Migrate soem from mcu-sdk-2.0 to a standalone repo
    Move boards/src folder to middleware/soem.
    
    Signed-off-by: qizhang <jony.zhang@nxp.com>

---
+++ example/soem_gpio_pulse/freertos/soem_gpio_pulse.c
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2022 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "fsl_gpio.h"
+#include "fsl_iomuxc.h"
+#include "fsl_gpt.h"
+#include "fsl_phyksz8081.h"
+#include "fsl_debug_console.h"
+
+#include "nicdrv.h"
+#include "ethercattype.h"
+#include "ethercatbase.h"
+#include "ethercatmain.h"
+#include "ethercatdc.h"
+#include "ethercatcoe.h"
+#include "ethercatfoe.h"
+#include "ethercatconfig.h"
+#include "ethercatprint.h"
+#include "enet/soem_enet.h"
+#include "soem_port.h"
+#include "FreeRTOS.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+#define NUM_1M (1000000UL)
+
+#define OSEM_PORT_NAME "enet0"
+#define EC_TIMEOUTMON  500
+
+#define SOEM_PERIOD        125 /* 125us */
+#define RT_TASK_STACK_SIZE 1024
+
+#define ENET_RXBD_NUM (4)
+#define ENET_TXBD_NUM (4)
+
+#define ENET_RXBUFF_SIZE (ENET_FRAME_MAX_FRAMELEN)
+#define ENET_TXBUFF_SIZE (ENET_FRAME_MAX_FRAMELEN)
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*! @brief Buffer descriptors should be in non-cacheable region and should be align to "ENET_BUFF_ALIGNMENT". */
+AT_NONCACHEABLE_SECTION_ALIGN(static enet_rx_bd_struct_t g_rxBuffDescrip[ENET_RXBD_NUM], ENET_BUFF_ALIGNMENT);
+AT_NONCACHEABLE_SECTION_ALIGN(static enet_tx_bd_struct_t g_txBuffDescrip[ENET_TXBD_NUM], ENET_BUFF_ALIGNMENT);
+
+/*! @brief The data buffers can be in cacheable region or in non-cacheable region.
+ * If use cacheable region, the alignment size should be the maximum size of "CACHE LINE SIZE" and "ENET_BUFF_ALIGNMENT"
+ * If use non-cache region, the alignment size is the "ENET_BUFF_ALIGNMENT".
+ */
+AT_NONCACHEABLE_SECTION_ALIGN(
+    static uint8_t g_rxDataBuff[ENET_RXBD_NUM][SDK_SIZEALIGN(ENET_RXBUFF_SIZE, ENET_BUFF_ALIGNMENT)],
+    ENET_BUFF_ALIGNMENT);
+AT_NONCACHEABLE_SECTION_ALIGN(
+    static uint8_t g_txDataBuff[ENET_TXBD_NUM][SDK_SIZEALIGN(ENET_TXBUFF_SIZE, ENET_BUFF_ALIGNMENT)],
+    ENET_BUFF_ALIGNMENT);
+
+static enet_buffer_config_t buffConfig[] = {{
+    ENET_RXBD_NUM,
+    ENET_TXBD_NUM,
+    SDK_SIZEALIGN(ENET_RXBUFF_SIZE, ENET_BUFF_ALIGNMENT),
+    SDK_SIZEALIGN(ENET_TXBUFF_SIZE, ENET_BUFF_ALIGNMENT),
+    &g_rxBuffDescrip[0],
+    &g_txBuffDescrip[0],
+    &g_rxDataBuff[0][0],
+    &g_txDataBuff[0][0],
+    true,
+    true,
+    NULL,
+}};
+
+static char IOmap[100];
+
+static StackType_t IdleTaskStack[configMINIMAL_STACK_SIZE];
+static StaticTask_t IdleTaskTCB;
+
+static StackType_t TimerTaskStacj[configMINIMAL_STACK_SIZE];
+static StaticTask_t TimerTaskTCB;
+
+static StaticTask_t xTaskBuffer;
+static TaskHandle_t rt_task = NULL;
+static StackType_t rt_task_stack[RT_TASK_STACK_SIZE];
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize)
+{
+    *ppxIdleTaskTCBBuffer   = &IdleTaskTCB;
+    *ppxIdleTaskStackBuffer = &IdleTaskStack[0];
+    *pulIdleTaskStackSize   = configMINIMAL_STACK_SIZE;
+}
+
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize)
+{
+    *ppxTimerTaskTCBBuffer   = &TimerTaskTCB;
+    *ppxTimerTaskStackBuffer = &TimerTaskStacj[0];
+    *pulTimerTaskStackSize   = configMINIMAL_STACK_SIZE;
+}
+
+void irq_wake_task(void)
+{
+    BaseType_t xHigherPriorityTaskWoken;
+
+    if (rt_task)
+    {
+        xHigherPriorityTaskWoken = pdFALSE;
+        vTaskNotifyGiveFromISR(rt_task, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
+
+void control_task(void *ifname)
+{
+    int oloop, iloop;
+    int expectedWKC;
+    volatile int wkc;
+    int old_switch0 = 0;
+    int old_switch1 = 0;
+    struct timeval target_time;
+    const TickType_t xBlockTime = pdMS_TO_TICKS(500);
+    char chk                    = 40;
+
+    /* initialise SOEM, and if_port */
+    if (ec_init(ifname))
+    {
+        PRINTF("ec_init on %s succeeded.\r\n", ifname);
+        if (ec_config_init(FALSE) > 0)
+        {
+            PRINTF("%d slaves found and configured.\r\n", ec_slavecount);
+            ec_config_map(&IOmap);
+            ec_configdc();
+            PRINTF("Slaves mapped, state to SAFE_OP.\r\n");
+            /* wait for all slaves to reach SAFE_OP state */
+            ec_statecheck(0, EC_STATE_SAFE_OP, EC_TIMEOUTSTATE * 4);
+            oloop = ec_slave[2].Obytes;
+            iloop = ec_slave[3].Ibytes;
+            PRINTF("oloop = %d, iloop = %d\r\n", oloop, iloop);
+            PRINTF("segments : %d : %d %d %d %d\r\n", ec_group[0].nsegments, ec_group[0].IOsegment[0],
+                   ec_group[0].IOsegment[1], ec_group[0].IOsegment[2], ec_group[0].IOsegment[3]);
+
+            PRINTF("Request operational state for all slaves\r\n");
+            expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
+            PRINTF("Calculated workcounter %d\r\n", expectedWKC);
+            ec_slave[0].state = EC_STATE_OPERATIONAL;
+            /* send one valid process data to make outputs in slaves happy*/
+            ec_send_processdata();
+            ec_receive_processdata(EC_TIMEOUTRET);
+            /* request OP state for all slaves */
+            ec_writestate(0);
+            /* wait for all slaves to reach OP state */
+            do
+            {
+                ec_send_processdata();
+                ec_receive_processdata(EC_TIMEOUTRET);
+                ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
+            } while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL));
+            PRINTF("Operational state reached for all slaves.\r\n");
+            /* cyclic loop */
+            ulTaskNotifyTake(pdTRUE, xBlockTime);
+            osal_gettime(&target_time);
+            for (;;)
+            {
+                ec_send_processdata();
+                wkc = ec_receive_processdata(EC_TIMEOUTRET);
+                if (wkc >= expectedWKC)
+                {
+                    if ((*(ec_slave[3].inputs) & 0x01) && old_switch0 == 0)
+                    {
+                        if (*(ec_slave[2].outputs) & 0x04)
+                            *(ec_slave[2].outputs) &= ~0x04;
+                        else
+                            *(ec_slave[2].outputs) |= 0x04;
+                    }
+
+                    if ((*(ec_slave[3].inputs) & 0x02) && old_switch1 == 0)
+                    {
+                        if (*(ec_slave[2].outputs) & 0x04)
+                            *(ec_slave[2].outputs) &= ~0x04;
+                        else
+                            *(ec_slave[2].outputs) |= 0x04;
+                    }
+
+                    old_switch0 = *(ec_slave[3].inputs) & 0x01;
+                    old_switch1 = *(ec_slave[3].inputs) & 0x02;
+                    if (*(ec_slave[2].outputs) & 0x02)
+                        *(ec_slave[2].outputs) &= ~0x02;
+                    else
+                        *(ec_slave[2].outputs) |= 0x02;
+                }
+                ulTaskNotifyTake(pdFALSE, xBlockTime);
+            }
+        }
+    }
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    BOARD_InitHardware();
+
+    PRINTF("Start the soem_gpio_pulse FreeRTOS example...\r\n");
+
+    osal_timer_init(SOEM_PERIOD, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
+    if_port_init();
+    rt_task = xTaskCreateStatic(/* The function that implements the task. */
+                                control_task, "RT_task", RT_TASK_STACK_SIZE, OSEM_PORT_NAME, configMAX_PRIORITIES - 1,
+                                rt_task_stack, &xTaskBuffer);
+
+    vTaskStartScheduler();
+    for (;;)
+        ;
+    return 0;
+}
